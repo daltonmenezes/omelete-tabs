34a7516d335e01437596ff02038ee8387dbd2743
Dalton Menezes <daltonmenezes@outlook.com>
2018-12-06 13:14:31 -0300

    Update extensions to work without innerHTML for security reasons

--- firefox/content_scripts/renders/news-render.js
+++ firefox/content_scripts/renders/news-render.js
@@ -3,20 +3,40 @@ const newsRender = ({ url, title, category, img }, index = 0) => {
     title.length >= 75
       ? '...'
       : ''
+  
+  article = document.getElementById('news')
+  section = document.createElement('section')
 
-  document.getElementById('news').innerHTML +=
-  `
-  <section>
-    <a href="${ url }" title="${ title }" tabindex="${ index + 2 }">
-    <div class="tag">
-      <span>${ category }</span>
-      <div class="tag-label tag-label-${ tagLabelHandler(category) }"></div>
-    </div>
-      <div class="picture">
-        <img src="${ img }" alt=""/>
-      </div>
-      ${ title.slice(0, 74).trim() }${ canTruncate }
-    </a> 
-  </section>  
-  `
+  a = document.createElement('a')
+  a.setAttribute('href', url)
+  a.setAttribute('title', title)
+  a.setAttribute('tabindex', index + 2)
+  
+  divTag = document.createElement('div')
+  divTag.setAttribute('class', 'tag')
+  
+  spanTag = document.createElement('span')
+  spanTag.textContent = category
+  
+  divLabelTag = document.createElement('div')
+  divLabelTag.setAttribute('class', `tag-label tag-label-${ tagLabelHandler(category) }`)
+
+  divPicture = document.createElement('div')
+  divPicture.setAttribute('class', 'picture')
+  
+  imgPicture = document.createElement('img')
+  imgPicture.setAttribute('src', img)
+  imgPicture.setAttribute('alt', '')
+
+  article.appendChild(section)
+  section.appendChild(a)
+  a.appendChild(divTag)
+  divTag.appendChild(spanTag)
+  divTag.appendChild(divLabelTag)
+  a.appendChild(divPicture)
+  divPicture.appendChild(imgPicture)
+  
+  span = document.createElement('span')
+  span.textContent = title.slice(0, 74).trim() + canTruncate
+  a.appendChild(span)
 }
